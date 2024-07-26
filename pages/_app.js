92ced32311e5b3379acc6e87d70f96bf97a26d53
OberoiDhruv <dhruv.oberoi@gmail.com>
2024-07-26 13:12:47 +1000

    sender popup form

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
+import Head from 'next/head';
 import "../styles/tailwind.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+
+        {/* Responsive Meta */}
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=5"
+        />
+
+        {/* Additional head content can go here */}
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,10 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
-export default function MyDocument() {
+export default function Document() {
   return (
     <Html>
-      <Head />
-      <body>
+      <Head>
+        {/* Google Analytics */}
         <script
           async
           src="https://www.googletagmanager.com/gtag/js?id=G-L1QKPLTWQX"
@@ -12,16 +12,43 @@ export default function MyDocument() {
         <script
           dangerouslySetInnerHTML={{
             __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', 'G-L1QKPLTWQX');
-              `,
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-L1QKPLTWQX');
+            `,
           }}
         />
-        <script src="/posthog.js" />
+
+        {/* Responsive Meta */}
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=5"
+        />
+
+        {/* Sender Popup */}
+        <script
+          id="sender-script"
+          dangerouslySetInnerHTML={{
+            __html: `
+            (function (s, e, n, d, er) {
+              s['Sender'] = er;
+              s[er] = s[er] || function () {
+                (s[er].q = s[er].q || []).push(arguments)
+              }, s[er].l = 1 * new Date();
+              var a = e.createElement(n),
+                  m = e.getElementsByTagName(n)[0];
+              a.async = 1;
+              a.src = d;
+              m.parentNode.insertBefore(a, m)
+            })(window, document, 'script', 'https://cdn.sender.net/accounts_resources/universal.js', 'sender');
+            sender('aceb5a7aef2115')
+            `,
+          }}
+        />
+      </Head>
+      <body>
         <Main />
-        <div id="modal" />
         <NextScript />
       </body>
     </Html>
